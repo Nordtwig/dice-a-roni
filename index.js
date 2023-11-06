1c76ef067e44d883feb2c4e80cbbdbd788a1b2e7
Noah <noah.nordqvisten@gmail.com>
2023-11-06 18:14:40 +0100

    Add result prompt

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const prompt = document.getElementById("prompt");
 const player1Die = document.getElementById("img1");
 const player2Die = document.getElementById("img2");
 
+player1Roll = generateRandomInt();
+player2Roll = generateRandomInt();
 
+console.log(player1Roll);
+console.log(player2Roll);
 
-player1Die.src = `./img/dice${generateRandomInt()}.png`;
-player2Die.src = `./img/dice${generateRandomInt()}.png`;
+player1Die.src = `./img/dice${player1Roll}.png`;
+player2Die.src = `./img/dice${player2Roll}.png`;
+
+if (player1Roll === player2Roll) 
+    prompt.textContent = "Draw!";
+else if (player1Roll > player2Roll) 
+    prompt.textContent = "🚩 Player 1 Wins!";
+else if (player1Roll < player2Roll) 
+    prompt.textContent = "🚩 Player 2 Wins!";
+
 
 function generateRandomInt() {
     return Math.floor(Math.random() * (7 - 1) + 1);
